ab21446de17f6d9d9f6faabdbc20e8231c8f5b96
cmdskp <comdudeskip@hotmail.com>
2022-03-11 12:33:33 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,20 @@
+Minetest mod: repel
+Version: 1.0
+Author: cmdskp
+===================
+Depends:
+--------
+default
+
+Description:
+------------
+A Minetest 0.4.16 to 5.5.0 mod to repel players trying to dig through protected area blocks.
+It teleports them back to where they starting digging the protected node and zeros their acceleration.
+
+Can work with any protection mods, providing they are loaded first in the depends.txt.
+(Currently, both Protector and Areas mods are listed in the depends.txt to ensure they load first.)
+
+License of source code:
+-----------------------
+LGPLv2
+
---
+++ init.lua
@@ -0,0 +1,54 @@
+local last_positions = {}
+
+local function restore_player_position(a_name)
+	if a_name then
+		local temp_position = last_positions[a_name]
+		if temp_position then
+			local temp_player = core.get_player_by_name(a_name)
+			temp_player:setpos(temp_position)
+			temp_player:setacceleration({x=0, y=0, z=0})
+			last_positions[a_name] = nil
+		end
+	end
+end
+
+core.register_on_punchnode(function(a_position, a_node, a_puncher, a_pointed_thing)
+	if a_puncher then
+		local temp_player_name = a_puncher:get_player_name()
+		if temp_player_name then
+			if core.is_protected(a_position, temp_player_name) then
+				if last_positions[temp_player_name]==nil then
+					last_positions[temp_player_name] = a_puncher:getpos()
+				end
+			else
+				last_positions[temp_player_name] = nil
+			end
+		end
+	end
+end)
+
+local next = next
+
+core.register_globalstep(function(a_delta_time)
+	if next(last_positions)~=nil then
+		local temp_players = core.get_connected_players()
+		for _, temp_player in ipairs(temp_players) do
+			local temp_player_name = temp_player:get_player_name()
+			if not temp_player:get_player_control().LMB then
+				last_positions[temp_player_name] = nil
+			end
+		end
+	end
+end)
+
+local temp_is_protected = core.is_protected
+
+core.is_protected = function(a_position, a_digger_name, a_only_owner)
+	local result = temp_is_protected(a_position, a_digger_name, a_only_owner)
+	if result and a_digger_name then
+		restore_player_position(a_digger_name)
+	end
+	return result
+end
+
+print("[MOD] repel loaded!")
